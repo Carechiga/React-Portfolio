e5ab2e78c3d614ba4817b9cd5a8830134c49bc9d
Carechiga <Carechiga43@gmail.com>
2023-05-17 15:34:01 -0700

    created about page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,21 +4,23 @@ function Footer() {
     return (
         <footer className="footer">
             <div>
-                Chris's Github
                 <a href="https://github.com/carechiga" target="_blank" rel="noopener noreferrer">
                     <img src={require("../../assets/icons/github-icon.png")} alt="github" className="logo"></img>
                 </a>
             </div>
             <div>
-                Chris' LinkedIn
                 <a href="https://www.linkedin.com/in/christopher-arechiga-2b027473/" target="_blank" rel="noopener noreferrer">
                     <img src={require("../../assets/icons/linkedin-icon.webp")} alt="Linkedin" className="logo"></img>
                 </a>
             </div>
             <div>   
-                Chris' Youtube
                 <a href="https://www.youtube.com/channel/UC52_oOkPU_eeSwkBXDnZizw/" target="_blank" rel="noopener noreferrer">
                     <img src={require("../../assets/icons/youtube-icon.avif")} alt="youtube" className="logo"></img>
+                </a>
+            </div>
+            <div>   
+                <a href="https://open.spotify.com/artist/4EFPnmy04sQLIFhHKKw6rE" target="_blank" rel="noopener noreferrer">
+                    <img src={require("../../assets/icons/Spotify_icon.svg.png")} alt="spotifcy" className="logo"></img>
                 </a>
             </div>
         </footer>
